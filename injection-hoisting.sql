aec7b6022b729184edb88217bf0f86bb476ef0cf
rnglb <kashakkumari5@gmail.com>
2022-06-15 01:46:09 +0530

    code update

---
+++ injection-hoisting.sql
@@ -0,0 +1,36 @@
+
+SQL injection
+/*
+--Batched SQL Statements 
+--A batch of SQL statements is a group of two or more SQL statements, separated by semicolons.
+
+SQL injection is a code injection technique that might destroy your database.
+
+SQL injection is one of the most common web hacking techniques.
+
+SQL injection is the placement of malicious code in SQL statements, via web page input.
+
+SQL injection usually occurs when you ask a user for input, like their username/userid, 
+and instead of a name/id, the user gives you an SQL statement that you will unknowingly run on your database.
+
+To protect a web site from SQL injection, you can use SQL parameters.
+SQL parameters are values that are added to an SQL query at execution time, in a controlled manner.
+
+*/
+
+SQL Hosting
+/*
+If you want your web site to be able to store and retrieve data from a database,
+ your web server should have access to a database-system that uses the SQL language.
+
+ The most common SQL hosting databases are MS SQL Server, Oracle, MySQL, and MS Access.
+
+Oracle
+Oracle is also a popular database software for database-driven web sites with high traffic.
+Oracle is a very powerful, robust and full featured SQL database system.
+
+MySQL
+MySQL is also a popular database software for web sites.
+MySQL is a very powerful, robust and full featured SQL database system.
+MySQL is an inexpensive alternative to the expensive Microsoft and Oracle solutions.
+ */
